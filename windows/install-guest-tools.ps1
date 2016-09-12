b0f3323151649ead78045d817ad973303b72051a
sriharsha <sriharsha@cliqz.com>
2016-09-12 16:47:37 +0200

    guest tools console out

--- windows/install-guest-tools.ps1
+++ windows/install-guest-tools.ps1
@@ -1,7 +1,8 @@
 # Set the path of the VMWare Tools ISO - this is set in the Packer JSON file
 
-$isopath = "C:\Windows\Temp\windows.iso"
+$isopath = "c:\Windows\Temp\windows.iso"
 
+Write-Host "Installing guest tools"
 # Mount the .iso, then build the path to the installer by getting the Driveletter attribute from Get-DiskImage piped into Get-Volume and adding a :\setup64.exe
 # A separate variable is used for the parameters. There are cleaner ways of doing this. I chose the /qr MSI Installer flag because I personally hate silent installers
 # Even though our build is headless. 
@@ -10,11 +11,12 @@ $isopath = "C:\Windows\Temp\windows.iso"
 Mount-DiskImage -ImagePath $isopath
 
 
-$exe = ((Get-DiskImage -ImagePath $isopath | Get-Volume).Driveletter + ':\setup.exe')
+$exe = ((Get-DiskImage -ImagePath $isopath | Get-Volume).Driveletter + ':\setup64.exe')
+Write-Host "Installed guest tools"
 $parameters = '/S /v "/qr REBOOT=R"'
 
 Start-Process $exe $parameters -Wait
-
+Write-Host "Starting guest tools"
 
 #Time to clean up - dismount the image and delete the original ISO
 
