73b23295044ba157b557700551c5d1741676e4a5
sriharsha <sriharsha@cliqz.com>
2016-09-16 16:21:41 +0200

    java added

--- windows/remove-installers.ps1
+++ windows/remove-installers.ps1
@@ -3,4 +3,5 @@ Remove-Item c:\mozilla-build.zip
 Remove-Item c:\go1.6.3.windows-amd64.msi
 Remove-Item c:\vs2015_pro_adminfile
 Remove-Item c:\nss.zip
+Remove-Item c:\jre-8u101-windows-x64.exe
 Remove-Item $env:temp\* -Recurse -Force
