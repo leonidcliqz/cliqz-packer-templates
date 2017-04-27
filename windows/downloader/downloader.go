6c3c52c16e029614ea6c410afa775afeaf75ec71
Leonid Rashkovsky <leonid@cliqz.com>
2017-04-27 14:27:01 +0200

    Imlemented a downloader for s3 packages

---
+++ windows/downloader/downloader.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+    "log"
+    "io"
+    "os"
+    "strings"
+    "encoding/json"
+    "github.com/aws/aws-sdk-go/aws"
+    "github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+)
+
+type configuration struct {
+	PackagesDir string
+	Bucket string
+	KeyBase string
+	Packages []string
+}
+
+func getConfig() *Configuration {
+	file, _ := os.Open("config.json")
+	decoder := json.NewDecoder(file)
+	config := configuration{}
+	err := decoder.Decode(&config)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return &config
+}
+
+func download(bucket, packageKey, output string) {
+	sess := session.New()
+	svc := s3.New(sess, aws.NewConfig().WithRegion("us-east-1"))
+
+	results, err := svc.GetObject(&s3.GetObjectInput{
+		Bucket: aws.String(Bucket),
+		Key: aws.String(package_key),
+	})
+
+	if err != nil {
+		log.Fatal(err)
+		os.Exit(1)
+	}
+
+	defer results.Body.Close()
+
+    packageFile, err := os.Create(output)	
+    
+    if err != nil {
+    	log.Fatal("Failed to create file", err)
+    }
+
+    defer packageFile.Close()
+
+	if _, err := io.Copy(packageFile, results.Body); err != nil {
+		log.Fatal(err)		
+	}
+}
+
+func createPackageDir(packagesDir string) error {
+	if _, err := os.Stat(packagesDir); err != nil {
+        if os.IsNotExist(err) {
+            err := os.Mkdir(packagesDir, 0777)
+            log.Fatal(err)
+            return err
+        }
+    }
+    return nil
+}
+
+func main() {
+	config := getConfig()
+
+    err := createPackageDir(config.PackagesDir)
+    if err != nil {
+    	log.Fatal(err)
+    }
+
+    for _, v := range config.Packages {
+		packageKey := strings.Join([]string{config.KeyBase, v}, "/")
+        output := strings.Join([]string{config.PackagesDir, v}, "/")
+
+        log.Printf("Downloading %v", v)
+		download(config.Bucket, packageKey, output)
+	}
+}
