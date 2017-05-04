e32c2a836fb5c6f757e6a901581d27a5c5a4fbc8
Leonid Rashkovsky <leonid@cliqz.com>
2017-05-04 15:07:55 +0200

    Reverted some changes related to user-data

--- windows/ec2-userdata.ps1
+++ windows/ec2-userdata.ps1
@@ -3,9 +3,6 @@
 write-output "Running user data script"
 write-host "(host) Running user data script"
 
-net user /add vagrant VerySecuredPassword
-net localgroup administrators vagrant /add
-
 Set-ExecutionPolicy -ExecutionPolicy bypass -Force
 
 # Supress network location Prompt
@@ -17,21 +14,10 @@ Set-NetConnectionProfile -InterfaceIndex $ifaceinfo.InterfaceIndex -NetworkCateg
 
 # WinRM 
 winrm quickconfig -q
-winrm quickconfig '-transport:http'
-winrm set "winrm/config" '@{MaxTimeoutms="1800000"}'
-winrm set "winrm/config/winrs" '@{MaxMemoryPerShellMB="512"}'
-winrm set "winrm/config/service" '@{AllowUnencrypted="true"}'
-winrm set "winrm/config/client" '@{AllowUnencrypted="true"}'
-winrm set "winrm/config/service/auth" '@{Basic="true"}'
-winrm set "winrm/config/client/auth" '@{Basic="true"}'
-winrm set "winrm/config/service/auth" '@{CredSSP="true"}'
-winrm set "winrm/config/listener?Address=*+Transport=HTTP" '@{Port="5985"}'
-netsh advfirewall firewall set rule group="remote administration" new enable=yes 
-netsh firewall add portopening TCP 5985 "Port 5985" 
-net stop winrm 
-sc config winrm start= auto
-net start winrm
-wmic useraccount where "name='vagrant'" set PasswordExpires=FALSE
+winrm s "winrm/config" '@{MaxTimeoutms="1800000"}'
+winrm s "winrm/config/winrs" '@{MaxMemoryPerShellMB="2048"}'
+winrm s "winrm/config/service" '@{AllowUnencrypted="true"}'
+winrm s "winrm/config/service/auth" '@{Basic="true"}'
 
 # RDP
 netsh advfirewall firewall add rule name="Open Port 3389" dir=in action=allow protocol=TCP localport=3389
@@ -41,6 +27,9 @@ reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Terminal Server" /v
 Enable-NetFirewallRule -DisplayName "Windows Remote Management (HTTP-In)"
 
 sc.exe config winrm start= auto
+
+net user /add vagrant VerySecuredPassword
+net localgroup administrators vagrant /add
 
 exit 0
 
