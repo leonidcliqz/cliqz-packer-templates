cda751d00de8de7e2a9fad01c7430bd7a4234c76
Leonid Rashkovsky <leonid@cliqz.com>
2017-05-04 14:32:20 +0200

    Removed calls to cmd.exe in userdata file

--- windows/ec2-userdata.ps1
+++ windows/ec2-userdata.ps1
@@ -3,8 +3,8 @@
 write-output "Running user data script"
 write-host "(host) Running user data script"
 
-cmd.exe /c net user /add vagrant VerySecuredPassword
-cmd.exe /c net localgroup administrators vagrant /add
+net user /add vagrant VerySecuredPassword
+net localgroup administrators vagrant /add
 
 Set-ExecutionPolicy -ExecutionPolicy bypass -Force
 
@@ -16,26 +16,26 @@ $ifaceinfo = Get-NetConnectionProfile
 Set-NetConnectionProfile -InterfaceIndex $ifaceinfo.InterfaceIndex -NetworkCategory Private
 
 # WinRM 
-cmd.exe /c winrm quickconfig -q
-cmd.exe /c winrm quickconfig '-transport:http'
-cmd.exe /c winrm set "winrm/config" '@{MaxTimeoutms="1800000"}'
-cmd.exe /c winrm set "winrm/config/winrs" '@{MaxMemoryPerShellMB="512"}'
-cmd.exe /c winrm set "winrm/config/service" '@{AllowUnencrypted="true"}'
-cmd.exe /c winrm set "winrm/config/client" '@{AllowUnencrypted="true"}'
-cmd.exe /c winrm set "winrm/config/service/auth" '@{Basic="true"}'
-cmd.exe /c winrm set "winrm/config/client/auth" '@{Basic="true"}'
-cmd.exe /c winrm set "winrm/config/service/auth" '@{CredSSP="true"}'
-cmd.exe /c winrm set "winrm/config/listener?Address=*+Transport=HTTP" '@{Port="5985"}'
-cmd.exe /c netsh advfirewall firewall set rule group="remote administration" new enable=yes 
-cmd.exe /c netsh firewall add portopening TCP 5985 "Port 5985" 
-cmd.exe /c net stop winrm 
-cmd.exe /c sc config winrm start= auto
-cmd.exe /c net start winrm
-cmd.exe /c wmic useraccount where "name='vagrant'" set PasswordExpires=FALSE
+winrm quickconfig -q
+winrm quickconfig '-transport:http'
+winrm set "winrm/config" '@{MaxTimeoutms="1800000"}'
+winrm set "winrm/config/winrs" '@{MaxMemoryPerShellMB="512"}'
+winrm set "winrm/config/service" '@{AllowUnencrypted="true"}'
+winrm set "winrm/config/client" '@{AllowUnencrypted="true"}'
+winrm set "winrm/config/service/auth" '@{Basic="true"}'
+winrm set "winrm/config/client/auth" '@{Basic="true"}'
+winrm set "winrm/config/service/auth" '@{CredSSP="true"}'
+winrm set "winrm/config/listener?Address=*+Transport=HTTP" '@{Port="5985"}'
+netsh advfirewall firewall set rule group="remote administration" new enable=yes 
+netsh firewall add portopening TCP 5985 "Port 5985" 
+net stop winrm 
+sc config winrm start= auto
+net start winrm
+wmic useraccount where "name='vagrant'" set PasswordExpires=FALSE
 
 # RDP
-cmd.exe /c netsh advfirewall firewall add rule name="Open Port 3389" dir=in action=allow protocol=TCP localport=3389
-cmd.exe /c reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Terminal Server" /v fDenyTSConnections /t REG_DWORD /d 0 /f
+netsh advfirewall firewall add rule name="Open Port 3389" dir=in action=allow protocol=TCP localport=3389
+reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Terminal Server" /v fDenyTSConnections /t REG_DWORD /d 0 /f
 
 # Enable the WinRM Firewall rule, which will likely already be enabled due to the 'winrm quickconfig' command above
 Enable-NetFirewallRule -DisplayName "Windows Remote Management (HTTP-In)"
